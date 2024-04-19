271785640f4d562a360df7deaf4609919387be83
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:18:10 +0330

    Generic model for API responses.

--- SimpleAuthNet/Services/AppResponse.cs
+++ SimpleAuthNet/Services/AppResponse.cs
@@ -1,6 +1,50 @@
 namespace SimpleAuthNet.Services;
 
-public class AppResponse
+public class AppResponse<T>
 {
-    
+    public bool IsSucceed { get; private set; } = true;
+    private Dictionary<string, string[]> Messages { get; set; } = [];
+
+    public T? Data { get; private set; }
+    public AppResponse<T> SetSuccessResponse(T data)
+    {
+        Data = data;
+        return this;
+    }
+    public AppResponse<T> SetSuccessResponse(T data, string key, string value)
+    {
+        Data = data;
+        Messages.Add(key, [value]);
+        return this;
+    }
+    public AppResponse<T> SetSuccessResponse(T data, Dictionary<string, string[]> message)
+    {
+        Data = data;
+        Messages = message;
+        return this;
+    }
+    public AppResponse<T> SetSuccessResponse(T data, string key, string[] value)
+    {
+        Data = data;
+        Messages.Add(key, value);
+        return this;
+    }
+    public AppResponse<T> SetErrorResponse(string key, string value)
+    {
+        IsSucceed = false;
+        Messages.Add(key, [value]);
+        return this;
+    }
+    public AppResponse<T> SetErrorResponse(string key, string[] value)
+    {
+        IsSucceed = false;
+        Messages.Add(key, value);
+        return this;
+    }
+    public AppResponse<T> SetErrorResponse(Dictionary<string, string[]> message)
+    {
+        IsSucceed = false;
+        Messages = message;
+        return this;
+    }
 }

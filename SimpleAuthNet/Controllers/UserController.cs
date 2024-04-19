4ee68dd1bd046e9d33bd31246bf95e8c3ca7d461
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:10:36 +0330

    Custom UserController added to handle user endpoints.

--- SimpleAuthNet/Controllers/UserController.cs
+++ SimpleAuthNet/Controllers/UserController.cs
@@ -1,6 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAuthNet.Services;
+
 namespace SimpleAuthNet.Controllers;
 
-public class UserController
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class UserController(UserService userService) : ControllerBase
 {
-    
+    [HttpPost]
+    public async Task<AppResponse<bool>> Register(UserRegisterRequest req)
+    {
+        return await userService.UserRegisterAsync(req);
+    }
+
+    [HttpPost]
+    public async Task<AppResponse<UserLoginResponse>> Login(UserLoginRequest req)
+    {
+        return await userService.UserLoginAsync(req);
+    }
+
+    [HttpPost]
+    public async Task<AppResponse<UserRefreshTokenResponse>> RefreshToken(UserRefreshTokenRequest req)
+    {
+        return await userService.UserRefreshTokenAsync(req);
+    }
+    [HttpPost]
+    public async Task<AppResponse<bool>> Logout()
+    {
+        return await userService.UserLogoutAsync(User);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public string Profile()
+    {
+        return User.FindFirst("UserName")?.Value ?? "";
+    }
 }

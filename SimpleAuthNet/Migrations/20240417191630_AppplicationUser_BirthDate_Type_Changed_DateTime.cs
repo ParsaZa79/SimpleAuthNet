1bf1d7b2203abcdf40ece9cecdf9e92efb38f021
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:13:11 +0330

    Migrations added for the models.

---
+++ SimpleAuthNet/Migrations/20240417191630_AppplicationUser_BirthDate_Type_Changed_DateTime.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleAuthNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppplicationUser_BirthDate_Type_Changed_DateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

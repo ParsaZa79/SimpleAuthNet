304237cf8e8b131512b8d893ebad5f9b24c8ed91
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:15:25 +0330

    A default weather forecast controller to test authorization.

--- SimpleAuthNet/Controllers/WeatherForecastController.cs
+++ SimpleAuthNet/Controllers/WeatherForecastController.cs
@@ -1,6 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleAuthNet.Models.Dto.Response;
+
 namespace SimpleAuthNet.Controllers;
 
-public class WeatherForcastController
+[ApiController]
+[Route("/api/[controller]")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class WeatherForecastController : ControllerBase
 {
-    
+    [HttpGet]
+    public IEnumerable<WeatherForecastModel> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecastModel(
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            Summaries[Random.Shared.Next(Summaries.Length)]
+        )).ToArray();
+    }
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 }

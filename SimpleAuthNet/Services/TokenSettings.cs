97f9920bf13c3c79f614cb7b79393435ff3f9980
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:18:26 +0330

    Model for reading token settings.

--- SimpleAuthNet/Services/TokenSettings.cs
+++ SimpleAuthNet/Services/TokenSettings.cs
@@ -10,5 +10,4 @@ public class TokenSettings
     public bool ValidateIssuer { get; set; } = true;
     public bool ValidateAudience { get; set; } = true;
     public bool ValidateLifetime { get; set; } = true;
-
 }

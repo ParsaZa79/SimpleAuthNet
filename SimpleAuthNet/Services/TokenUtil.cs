c36bdacc7a004a24650cc5cf1f09b5e266fe866c
Parsa Zeinali <pzeinali4@gmail.com>
2024-04-19 15:18:58 +0330

    A static util class for issuing tokens.

--- SimpleAuthNet/Services/TokenUtil.cs
+++ SimpleAuthNet/Services/TokenUtil.cs
@@ -1,6 +1,52 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using SimpleAuthNet.Models.Entities;
+
 namespace SimpleAuthNet.Services;
 
-public class TokenUtil
-{
-    
-}
+    public static class TokenUtil
+    {
+
+        public static string GetToken(TokenSettings appSettings, ApplicationUser user, List<Claim> roleClaims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.SecretKey));
+            var signInCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var userClaims = new List<Claim>
+            {
+                new("Id", user.Id.ToString()),
+                new ("UserName", user.UserName??"")
+            };
+            userClaims.AddRange(roleClaims);
+            var tokeOptions = new JwtSecurityToken(
+                issuer: appSettings.Issuer,
+                audience: appSettings.Audience,
+                claims: userClaims,
+                expires: DateTime.UtcNow.AddSeconds(appSettings.TokenExpireSeconds),
+                signingCredentials: signInCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return tokenString;
+        }
+
+        public static ClaimsPrincipal GetPrincipalFromExpiredToken(TokenSettings appSettings, string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.Audience,
+                ValidIssuer = appSettings.Issuer,
+                ValidateLifetime = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettings.SecretKey))
+            };
+
+            var principal = new JwtSecurityTokenHandler().ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("GetPrincipalFromExpiredToken Token is not validated");
+
+            return principal;
+        }
+    }
